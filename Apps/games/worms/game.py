bedd342cea7070320e0e1735649b87b4af9f00e5
Alexis <alexis_gavriel@hotmail.com>
2020-09-14 12:49:32 -0600

    pygame zoom changes

--- Apps/games/worms/game.py
+++ Apps/games/worms/game.py
@@ -5,66 +5,76 @@ pygame.init()
 
 
 class Player:
-    def __init__(self):
-        self.image = pygame.Surface((16, 16))  # Create Player Image
+    def __init__(self,x,y):
+        self.size = 16
+        self.image = pygame.Surface((self.size, self.size))  # Create Player Image
         self.image.fill(colors["RED"])  # Fill Player Red
-        self.rect = pygame.Rect((50, 50), (16, 16))  # Create Player Rect
+        self.rect = pygame.Rect((x, y), (16, 16))  # Create Player Rect
         self.zoom = 1
+        self.speed = 8
+        self.camera = -1*x//2,-1*y//2
 
-    def move_camera(self, camera_pos, dir):
-        pos_x, pos_y = camera_pos  # Split camara_pos
+    def move_camera(self, dir_):
+        pos_x, pos_y = self.camera  # Split camara_pos
 
         if dir_ == 1:
-            pos_y += 8  # Move Camara Coord Against Player Rect
+            pos_y += self.speed  # Move Camara Coord Against Player Rect
         if dir_ == 2:
-            pos_x += 8
+            pos_x += self.speed
         if dir_ == 3:
-            pos_y -= 8
+            pos_y -= self.speed
         if dir_ == 4:
-            pos_x -= 8
-        
+            pos_x -= self.speed
 
-    def move(self, camera_pos):
-        pos_x, pos_y = camera_pos  # Split camara_pos
-        #
-        key = pygame.key.get_pressed()  # Get Keyboard Input
-        if key[pygame.K_w]:  # Check Key
-            self.rect.y -= 8  # Move Player Rect Coord
-            pos_y += 8  # Move Camara Coord Against Player Rect
-        if key[pygame.K_a]:
-            self.rect.x -= 8
-            pos_x += 8
-        if key[pygame.K_s]:
-            self.rect.y += 8
-            pos_y -= 8
-        if key[pygame.K_d]:
-            self.rect.x += 8
-            pos_x -= 8
-        if key[pygame.K_q]:
-            self.zoom -= 0.1
-        elif key[pygame.K_e]:
-            self.zoom += 0.1
+        if self.rect.x < W_WIDTH//2:  # Simple Sides Collision            
+            pos_x = self.camera[0]  # Reset Camera Pos Coord        
+        elif self.rect.x > WIDTH - W_WIDTH//2:            
+            pos_x = self.camera[0]
+        if self.rect.y < W_HEIGHT//2:
+            pos_y = self.camera[1]
+        elif self.rect.y > HEIGHT - W_HEIGHT//2 :            
+            pos_y = self.camera[1]
+
+
+        self.camera = pos_x, pos_y
+        return self.camera
+        
+    def zoomin(self,val):
+        if val == 5:
+            self.zoom -= 1
+        else:
+            self.zoom += 1        
 
         if self.zoom < 1:
             self.zoom = 1
-        elif self.zoom > 3:
-            self.zoom = 3
-            
-        #
+        elif self.zoom > 2:
+            self.zoom = 2
+
+    def move(self, dir_):
+
+        print(f"old {self.rect.x} , {self.rect.y}", end= " - ")
+
+        if dir_ == 1:
+            self.rect.y -= self.speed
+        elif dir_ == 2:
+            self.rect.x -= self.speed
+        elif dir_ == 3:
+            self.rect.y += self.speed
+        else:
+            self.rect.x += self.speed
+
         if self.rect.x < 0:  # Simple Sides Collision
-            self.rect.x = 0  # Reset Player Rect Coord
-            pos_x = camera_pos[0]  # Reset Camera Pos Coord
-        elif self.rect.x > 984:
-            self.rect.x = 984
-            pos_x = camera_pos[0]
+            self.rect.x = 0  # Reset Player Rect Coord            
+        elif self.rect.x > WIDTH - self.size :
+            self.rect.x = WIDTH - self.size           
         if self.rect.y < 0:
             self.rect.y = 0
-            pos_y = camera_pos[1]
-        elif self.rect.y > 984:
-            self.rect.y = 984
-            pos_y = camera_pos[1]
-        #
-        return (pos_x, pos_y)  # Return New Camera Pos
+        elif self.rect.y > HEIGHT - self.size:
+            self.rect.y = HEIGHT - self.size
+
+        print(f"new {self.rect.x} , {self.rect.y}")
+
+        return self.move_camera(dir_)
 
     def render(self, display):
         display.blit(self.image, (self.rect.x, self.rect.y))
@@ -82,8 +92,8 @@ def Main(display, clock):
         # Put Blue Rectagles On Map Surface
         pygame.draw.rect(world, colors["BLUE"], ((x*100, x*100), (20, 20)))
     #
-    player = Player()  # Initialize Player Class
-    camera_pos = (192, 192)  # Create Camara Starting Position
+    player = Player(W_WIDTH,W_HEIGHT)  # Initialize Player Class
+    camera_pos = -1*W_WIDTH//2,-1*W_HEIGHT//2  # Create Camara Starting Position
     #
     while True:
         clock.tick(60)
@@ -91,9 +101,23 @@ def Main(display, clock):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
+        key = pygame.key.get_pressed()  # Get Keyboard Input
+        if key[pygame.K_w]:  # Check Key
+            player.move(1)                
+        if key[pygame.K_a]:
+            player.move(2)                
+        if key[pygame.K_s]:
+            player.move(3)                
+        if key[pygame.K_d]:
+            player.move(4)                
+        if key[pygame.K_q]:
+            player.zoomin(5)
+        elif key[pygame.K_e]:
+            player.zoomin(6)
         #
         # Run Player Move Function And Return New Camera Pos
-        camera_pos = player.move(camera_pos)
+        
         #
         # Fill The Background White To Avoid Smearing
         #display.fill(colors["WHITE"])
@@ -104,11 +128,14 @@ def Main(display, clock):
             pygame.draw.rect(world, colors["BLUE"], ((x*100, x*100), (20, 20)))
         player.render(world)  # Render The Player
 
-  
-        frame = pygame.transform.scale(world, (int(WIDTH/player.zoom), int(HEIGHT/player.zoom)))
-        display.blit(frame, frame.get_rect())
-        
-        #  display.blit(world, camera_pos)  # Render Map To The Display
+        if player.zoom > 1:
+            frame = pygame.transform.scale(world, (W_WIDTH, W_HEIGHT))
+            display.blit(frame, frame.get_rect())
+        else:
+            world_padded = pygame.Surface((WIDTH+W_WIDTH, HEIGHT+W_HEIGHT))
+            world_padded.fill(colors["GREEN"])
+            display.blit(world_padded,(0,0))
+            display.blit(world, player.camera)  # Render Map To The Display
         #
         pygame.display.flip()
 
@@ -116,11 +143,13 @@ def Main(display, clock):
 ###
 #
 if __name__ in "__main__":
-    display = pygame.display.set_mode((640, 360))
-    image_bg = pygame.image.load(r'test.png').convert()
 
-    WIDTH, HEIGHT = (1920,1080)
+    WIDTH, HEIGHT = 1920,1080
+    SCALE_ZOOM = 2
+    W_WIDTH, W_HEIGHT = WIDTH//SCALE_ZOOM , HEIGHT//SCALE_ZOOM
+    display = pygame.display.set_mode((W_WIDTH, W_HEIGHT))
     
+    image_bg = pygame.image.load(r'test.png').convert()
     pygame.display.set_caption("Scrolling Camera")
     clock = pygame.time.Clock()
     #
