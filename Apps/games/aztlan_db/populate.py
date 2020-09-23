5bef28518fec3f8ec95182342b35be00c0a7d4ac
Alexis <alexis_gavriel@hotmail.com>
2020-09-23 12:07:00 -0600

    Insert provincia, canton, distrito
    listas de canton, distrito por provincia

--- Apps/games/aztlan_db/populate.py
+++ Apps/games/aztlan_db/populate.py
@@ -1,43 +1,53 @@
 
-def create_provincia(conn, project):
+def create_provincia(conn, provincia, pais):
     """
-    Create a new project into the projects table
+    Crea nueva pronvincia
     :param conn:
-    :param project:
-    :return: project id
+    :param pronvincia:
+    :param pais:
+    
     """
-    sql = ''' INSERT INTO Provincia(Name, fkPais)
-              VALUES (?,?) '''
+
+    
+    #sql = ''' INSERT INTO Provincia(Name, fkPais)
+    #          VALUES (?,?) '''
+
+    sql = ''' INSERT into Provincia (Name, fkPais) values (?, 
+        (select idPais from Pais where Pais.Name = ? limit 1)) '''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, provincia, pais)
     conn.commit()
     return cur.lastrowid
 
 
-def create_canton(conn, canton, provincia):
+def create_canton(conn, canton, provincia, pais):
     """
-    Create a new project into the projects table
-    :param conn:
-    :param project:
-    :return: project id
+  
     """
-    sql = ''' INSERT INTO Provincia(Name, fkPais)
-              VALUES (?,?) '''
+    sql = ''' INSERT into Canton (Name, fkprovincia) values (?, 
+    (select idProvincia from Provincia where Provincia.Name = ? and Provincia.fkPais = 
+        (select idPais from Pais where Pais.Name = ? limit 1) 
+    limit 1)) '''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, canton, provincia, pais)
     conn.commit()
     return cur.lastrowid
 
-def create_distrito(conn, distrito, canton, provincia):
+def create_distrito(conn, distrito, canton, provincia, pais):
     """
     Create a new project into the projects table
     :param conn:
     :param project:
     :return: project id
     """    
-    sql = ''' INSERT INTO Provincia(Name, fkPais)
-              VALUES (?,?) '''
+    sql = ''' INSERT into Distrito(Name, fkCanton) values (?,
+    (select idCanton from Canton where Canton.Name = ? and Canton.fkProvincia = 
+        (select idProvincia from Provincia where Provincia.Name = ? and Provincia.fkPais = 
+            (select idPais from Pais where Pais.Name = ? limit 1) 
+        limit 1)
+    limit 1))'''
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, distrito, canton, provincia, pais)
     conn.commit()
     return cur.lastrowid
+
