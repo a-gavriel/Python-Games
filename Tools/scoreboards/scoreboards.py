6d430ed676912bb9b099d2e0b3026c7c2420012e
Alexis <alexis_gavriel@hotmail.com>
2021-09-12 12:03:18 -0600

    updating the scoreboard template

--- Tools/scoreboards/scoreboards.py
+++ Tools/scoreboards/scoreboards.py
@@ -4,7 +4,7 @@ import csv, json, random
 global scores
 scores = []
 
-
+# First row contains the header of the table
 def getindexscore(user_data):
     global scores
     if int(user_data[1]) > int(scores[1][1]):
@@ -20,12 +20,13 @@ def getindexscore(user_data):
     else:
         return -1
 
+# Inserts the user data in the given index, and deletes the last element
 def replacescore(user_data, index):
     global scores
     scores.insert(index,user_data)
     scores.pop(-1)
 
-
+# Loads the scores from the CSV
 def readcsv():
     global scores
     scores = []
@@ -36,40 +37,19 @@ def readcsv():
     
     csvFile.close()
 
-def main():
+def read_json():
     global scores
+    scores = [["Name","Score"]]
+    with open('scores.json', 'r') as jsonFile:
+        data = json.load(jsonFile)
+    jsonFile.close()
 
-    #la tabla
-    scores = [["Nombre","Score"]]+[["None","100"]]*5
-    print (scores)
-
-    loadCSV = True
-
-    if loadCSV:
-        readcsv()
-        print("changed scores from file:")
-        print(scores)
-
-    ## current user score and name
-    user_name = 'A' + str(random.randint(1, 100))
-    user_score = random.randint(1, 1000)
+    score_list = data['Scoreboard']
+    for score in score_list:
+        scores.append([score['Name'], score['Score']])
     
-    
-
-
-    ## places the player data in the top5 scoreboard
-
-    user_data = [user_name, str(user_score)]
-    print(user_data)
-    scoreboardindex = getindexscore(user_data)
-    if (scoreboardindex != -1):
-        replacescore(user_data, scoreboardindex)
-
-    print(scores)
-
-
-    ## generates CSV
-
+def csv_to_json():
+    # read CSV
     with open('scores.csv','w+', newline='') as outfilecsv: # w+ truncates file first
         writer = csv.writer(outfilecsv)
         writer.writerows(scores)
@@ -78,12 +58,54 @@ def main():
 
     ## generates JSON from CSV
     jsonfile = open("scores.json", "w+")
+    jsonfile.write('{\n "Scoreboard":')
     with open("scores.csv","r+") as csvfile:
         next(csvfile,None)
         reader = csv.DictReader(csvfile,fieldnames = ["Name","Score"])
-        json.dump([row for row in reader],jsonfile)
+        json.dump([row for row in reader], jsonfile, indent=4)
     
+    jsonfile.write('\n}')
     jsonfile.close()
     csvfile.close()
+
+def add_random_score():
+    ## current user score and name
+    user_name = 'A' + str(random.randint(1, 100))
+    user_score = random.randint(1, 1000)
+    ## Places the player data in the top5 scoreboard
+    user_data = [user_name, str(user_score)]
+    print("Inserting new user data: ",user_data)
+    scoreboardindex = getindexscore(user_data)
+    if (scoreboardindex != -1):
+        replacescore(user_data, scoreboardindex)
+    print(scores)
+
+
+def main():
+    global scores
+
+
+    default_values = True
+    if default_values:
+        scores = [["Name","Score"]]+[["None","0"]]*5
+        print("Using default scoreboard")
+        print(scores)
+
+    loadCSV = False
+    if loadCSV:
+        readcsv()
+        print("Loaded scores from CSV file:")
+        print(scores)
+    
+    loadJSON = False
+    if loadJSON:
+        read_json()
+        print("Loaded scores from JSON file:")
+        print(scores)
+
+    add_random_score()
+
+
+    #csv_to_json()
 
 main()
