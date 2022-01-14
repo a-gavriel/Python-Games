ed5aaaa48fe55102cbb2ba7ad24178951bc49cf7
Alexis <alexis_gavriel@hotmail.com>
2022-01-13 19:06:48 -0600

    simple telegram bot + scraper

---
+++ Apps/TelegramBot_Scraper/TelegramBot.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+import scraper
+
+bot_token = input("Please input bot token:")
+if len(bot_token) < 5:
+  print("Token error")
+  exit()
+
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+
+    if (update.message.text.lower() == "start thread"):
+      scraper.create_thread(update.message.reply_text)
+
+    elif (update.message.text.lower().find("timeout ") >= 0):
+      scraper.change_timeout(update.message.text , update.message.reply_text)
+
+    elif (update.message.text.lower() == "exit thread"):
+      scraper.thread_running = False
+
+    elif (update.message.text.lower() == "test"):
+      scraper.del_blacklist()
+      update.message.reply_text("Testing status: "+str(scraper.viagogo_test_blacklist))
+
+    elif (update.message.text.lower() == "status"):
+      result_list, results_log = scraper.check_pages()
+      update.message.reply_text(str(results_log))
+      
+    else:
+      update.message.reply_text("Command not found, message received:\n" + update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(bot_token)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+    scraper.thread_running = False
+    print("reached end.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Apps/TelegramBot_Scraper/scraper.py
@@ -0,0 +1,161 @@
+import requests
+import re
+import threading
+import time
+
+thread_running = False
+thread_var : threading.Thread
+thread_result = False
+viagogo_test_blacklist = False
+timeout_time = 300
+
+
+class Page:
+  def __init__(self, name, url, pattern, options):
+    self.name = name
+    self.url = url
+    self.text = ""
+    self.pattern = pattern
+    self.pattern_options = options
+    self.finds = []
+    #self.get_text()
+
+  def get_text(self):
+    self.text = ((requests.get(self.url)).text).lower()
+
+  def find(self):
+    if self.pattern != "":
+      if self.pattern_options == "":
+        self.finds = re.findall(self.pattern, self.text)
+      else:
+        self.finds = re.findall(self.pattern, self.text, self.pattern_options)
+
+  def filter_finds(self, pattern):
+    if self.finds != []:
+      for i, current in enumerate(self.finds):
+        m = re.match(pattern, current)
+        if m is not None:
+          self.finds.pop(i)
+
+  def __str__(self):
+    return (
+      f"""
+      Name:\t{self.name}
+      Url:\t{self.url[12:30]}...
+      Pattern:\t{self.pattern}
+      Finds:\t{self.finds}"""
+    )
+
+eticket = Page("eticket", "https://www.eticket.cr/eventos.aspx?categoria=1&", 
+                  ".{1,10}mägo de oz.{5,10}|.{1,10}mago de oz.{1,10}|.{1,10}mago.{1,10}", "" )
+
+viagogo =  Page("viagogo", "https://www.viagogo.com/ww/Concert-Tickets/Hard-Rock-Metal/Mago-De-Oz-Tickets", 
+                  ".{1,30}costa rica.{1,30}", "" )
+
+def check_pages():
+  global viagogo_test_blacklist
+
+  log = "Checking pages:\n"
+  results = []
+  try:
+    eticket.get_text()
+    viagogo.get_text()
+
+    log += "\nChecking: eticket\t"
+    eticket.find()
+    if (eticket.finds == []):
+      log += "None"
+    else:
+      log += f": {len(eticket.finds)} result(s)!"
+
+    log += "\nChecking: viagogo\t"
+    viagogo.find()
+    
+    if not viagogo_test_blacklist:
+      viagogo.filter_finds(".*currency.*")
+
+
+    if (viagogo.finds == []):
+      log += "None"
+    else:
+      log += f": {len(viagogo.finds)} result(s)!"
+
+
+    results.extend(eticket.finds)
+    results.extend(viagogo.finds)
+
+  except:
+    log += "Error getting data"
+
+
+  return(results, log)
+
+
+
+def del_blacklist() -> str:
+  """
+  Test function to invert viagogo's  blacklist function
+
+  """
+  global viagogo_test_blacklist
+  viagogo_test_blacklist = not viagogo_test_blacklist
+  print(viagogo_test_blacklist)
+  return str(viagogo_test_blacklist)
+
+
+
+def thread_function(reply_function):
+  """
+  Checks every second if should exit, if not check every "timeout time" the webpages
+  """
+  global thread_result, thread_running 
+  thread_result = False
+  while not thread_result:
+    for i in range(timeout_time):
+      time.sleep(1)
+      if not thread_running:
+        print("exiting")
+        return 
+    
+    print("Running Thread")
+    result = check_pages()
+    if result[0] != []:
+      reply_function("RESULT FOUND!")
+      reply_function(result[1])
+      thread_result = True
+      thread_running = False
+      return
+      
+  
+def change_timeout(message, reply_function) -> None:
+  """
+  Changes the timeout between requests to the different webpages
+  """
+  global timeout_time
+  timeout_time = 300  
+  try:
+    message = message.split()[1]
+    timeout_time = int(message)
+    if timeout_time < 1:
+      raise Exception("Not enough time")
+    reply_function("Set timeout:" + str(timeout_time))
+  except:
+    reply_function("Set timeout:" + str(timeout_time))
+
+
+
+def create_thread(reply_function) -> None:
+  """
+  Creates the thread that searches through the web pages
+  """
+  global thread_running, thread_var
+  if thread_running:
+    reply_function("Thread already running!")
+
+  else:
+    thread_running = True
+    thread_var = threading.Thread(target=thread_function, args=(reply_function,))
+    thread_var.start()
+    
+
+    
