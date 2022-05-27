965a3131e93e1e34728c8aa1986c15504fb513b5
Alexis <alexis_gavriel@hotmail.com>
2022-05-27 00:33:34 -0600

    donkey kong game changes

--- Apps/games/DonkeyKong/DonkeyKong.py
+++ Apps/games/DonkeyKong/DonkeyKong.py
@@ -1,7 +1,37 @@
 import tkinter as tk
 from constants import *
-from objects import Mario
+from objects import Mario, Paddle, Stair, Barrel
 
+
+
+
+def create_paddles(canvas : tk.Canvas) -> list[Paddle]:
+  paddle_list = [
+    Paddle(canvas, *PADDLE_1_COORDS),
+    Paddle(canvas, *PADDLE_2_COORDS),
+    Paddle(canvas, *PADDLE_3_COORDS),
+    Paddle(canvas, *PADDLE_4_COORDS),
+    Paddle(canvas, *PADDLE_5_COORDS),
+  ]
+
+  return paddle_list
+
+def create_stairs(canvas : tk.Canvas) -> list[Stair]:
+  stair_list = [
+    Stair(canvas, *STAIR_1_COORDS),
+    Stair(canvas, *STAIR_2_COORDS),
+    Stair(canvas, *STAIR_3_COORDS),
+    Stair(canvas, *STAIR_4_COORDS)
+  ]
+
+  return stair_list
+
+
+def update_all(window, barrel, paddle_list):
+
+  barrel.update(tk.Canvas , paddle_list )
+
+  window.after(10, update_all, window, paddle_list)
 
 
 def main():
@@ -13,16 +43,15 @@ def main():
   canvas.pack()
 
 
-  paddle_list = [
-    canvas.create_rectangle(*PADDLE_1_COORDS, fill="#7d0964"),
-    canvas.create_rectangle(*PADDLE_2_COORDS, fill="#7d0964"),
-    canvas.create_rectangle(*PADDLE_3_COORDS, fill="#7d0964"),
-    canvas.create_rectangle(*PADDLE_4_COORDS, fill="#7d0964"),
-    canvas.create_rectangle(*PADDLE_5_COORDS, fill="#7d0964"),
-  ]
+  paddle_list : list[Paddle] = create_paddles(canvas)
+  stair_list : list[Stair] = create_stairs(canvas)
+  
 
   player = Mario(canvas, paddle_list, 50, MARIO_Y1, 70, PADDLE_1_Y1)
-  
+  barrel = Barrel(canvas, 20, 90, paddle_list)
+
+  update_all(main_window, barrel, paddle_list)
+
   main_window.bind("<KeyPress-Left>", player.left)
   main_window.bind("<KeyPress-Right>", player.right)
   main_window.bind("<KeyPress-Up>", player.up)
--- Apps/games/DonkeyKong/objects.py
+++ Apps/games/DonkeyKong/objects.py
@@ -1,31 +1,98 @@
 import tkinter as tk
-
+import constants
 
 class Mario:
-  def __init__(self, map_canvas : tk.Canvas, paddle_list : list, 
-      x1 : int, y1 : int, x2 : int, y2 : int):
-    self.id_ : int = map_canvas.create_oval(x1, y1, x2, y2, fill = "#eb3737")
-    self.map_canvas : tk.Canvas = map_canvas
-    
+    def __init__(
+        self,
+        map_canvas: tk.Canvas,
+        paddle_list: list['Paddle'],
+        x1: int,
+        y1: int,
+        x2: int,
+        y2: int,
+    ):
+        self.id_: tk._CanvasItemId = map_canvas.create_oval(x1, y1, x2, y2, fill="#eb3737")
+        self.map_canvas: tk.Canvas = map_canvas
 
-    self.current_paddle : int = 0
-    self.speed = 10
+        self.current_paddle: int = 0
+        self.speed: int = 10
 
-  def left(self, event) -> None:
-    self.map_canvas.move(self.id_, -1*self.speed, 0)
-    return
+    def left(self, event) -> None:
+        self.map_canvas.move(self.id_, -1 * self.speed, 0)
+        return
 
-  def right(self, event) -> None:
-    self.map_canvas.move(self.id_, self.speed, 0)
-    return
+    def right(self, event) -> None:
+        self.map_canvas.move(self.id_, self.speed, 0)
+        return
 
-  def up(self, event) -> None:
-    print("climbing")
-    return
+    def up(self, event) -> None:
+        print("climbing")
+        return
 
-  def jump(self, event) -> None:
-    print("jumping")
-    return
+    def jump(self, event) -> None:
+        print("jumping")
+        return
 
-  def check_sides(self) -> bool:
-    pass
+    def check_sides(self) -> bool:
+        pass
+
+
+class Paddle:
+    def __init__(
+        self, canvas: tk.Canvas, x: int, y: int, w: int, h: int, color="#7D0964"
+    ):
+        self.x : int = x
+        self.y : int = y
+        self.w : int = w
+        self.h : int = h
+        self.x2 = x + w
+        self.id_ : tk._CanvasItemId = canvas.create_rectangle(x, y, x + w, y + h, fill=color)
+
+
+class Stair:
+    def __init__(
+        self, canvas: tk.Canvas, x: int, y: int, w: int, h: int, color="#097D77"
+    ):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.x2 = x + w
+        self.id_  : tk._CanvasItemId = canvas.create_rectangle(x, y, x + w, y + h, fill=color)
+
+
+class Barrel:
+    def __init__(
+        self, canvas : tk.Canvas, x:int, y:int, paddle_list : list[Paddle], w : int = 10, h : int = 10
+    ):
+        self.current_paddle : int = -len(paddle_list)        
+        self.vx : int = 10
+        self.vy : int = 0
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.falling = False
+        self.x2 = x + w
+        self.id_ : tk._CanvasItemId = canvas.create_oval(x, y, x+w, y+h )
+
+    def update(self, canvas : tk.Canvas, paddle_list : list[Paddle]) -> int:
+
+        self.x += self.vx
+        self.y += self.vy
+
+        tk.Canvas.move(canvas, self.id_, self.vx, self.vy)
+
+        if self.x >= constants.WIDTH or self.x <= 0:
+            self.vy *= -1
+            if self.current_paddle == 0:
+                return 1
+
+        if self.x >= paddle_list[self.current_paddle].x2 or self.x <= paddle_list[self.current_paddle].x:
+            self.falling = True
+
+        if self.falling and self.y >= (paddle_list[self.current_paddle + 1].y):
+            self.falling = False
+
+            
+        return 0
