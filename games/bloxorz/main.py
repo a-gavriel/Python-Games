2fc9883036f73f5bec7ae8ce605e460fc656873a
Alexis <alexis_gavriel@hotmail.com>
2022-09-20 00:31:04 -0600

    Bloxorz updating logic for a 3d game - Changed movement from square to block - Updated rendering to a shearing function instead of a rotation

--- games/bloxorz/main.py
+++ games/bloxorz/main.py
@@ -1,9 +1,10 @@
 import pygame, sys
 from numpy import random as np_rand
+import numpy
 
 class Game:
 
-  def get_matrix(size):
+  def get_matrix(size) -> numpy.array:
     matrix = np_rand.randint(0,20,(size,size))
     for i in range(size):
       for j in range(size):
@@ -11,17 +12,17 @@ class Game:
           matrix[i][j] = 1
     return matrix
 
-  def __init__(self,scale=40, matrix_size = 10):
-    self.crash = False       
-    self.score =  0
-    self.scale = scale
-    self.matrix_size = matrix_size
-    self.matrix = Game.get_matrix(matrix_size)
-    self.game_width = matrix_size*scale
-    self.game_height = self.game_width
-    self.display_offset = 0
+  def __init__(self, scale = 40, matrix_size = 10):
+    self.crash : bool = False       
+    self.score : int =  0
+    self.scale : int = scale
+    self.matrix_size : int = matrix_size
+    self.matrix : numpy.array = Game.get_matrix(matrix_size)
+    self.game_width : int = matrix_size*scale
+    self.game_height : int = self.game_width
+    self.display_offset : int = 0
     self.gameDisplay = pygame.display.set_mode((self.game_width,self.game_height+self.display_offset))
-    self.player = Player(0,0)
+    self.player : Player = Player(0,0)
     self.colors = {
       0: (255,255,255),
       1: (100,100,100)
@@ -31,8 +32,23 @@ class Game:
     pass
     #self.ball.update(self, self.player)
     #self.wall.update(self.ball,self)    
+
+
+  def shear2(a : numpy.array, strength=-1, shift_axis=0, increase_axis=1, edges='clip'):
+    indices = numpy.indices(a.shape)
+    indices[shift_axis] -= strength * indices[increase_axis]
+    indices[shift_axis] %= a.shape[shift_axis]
+    res = a[tuple(indices)]
+    if edges == 'clip':
+        res[indices[shift_axis] < 0] = 0
+        res[indices[shift_axis] >= a.shape[shift_axis]] = 0
+    return res
+
   def render(self):
     surface = pygame.Surface((self.game_width,self.game_height))
+    surface_bg = pygame.Surface((self.game_width,self.game_height))
+    surface_bg.fill((0,255,255))
+    self.gameDisplay.fill((255,255,255))
     
     surface.fill((0,255,255))
     for i,row in enumerate(self.matrix):
@@ -40,59 +56,124 @@ class Game:
         rect = pygame.Rect(( j*self.scale , i*self.scale , self.scale, self.scale)) #Creo el rectangulo, x1, y1, w, h
         pygame.draw.rect(surface,self.colors[tile],rect ) #Dibujo el rectangulo
  
-
-
     for i in range(self.game_width // self.scale):
       pygame.draw.line(surface, (255,255,255), (i*self.scale, 0), (i*self.scale, self.game_height))
       pygame.draw.line(surface, (255,255,255), (0, i*self.scale), (self.game_width, i*self.scale))
 
     self.player.render(surface, self.scale)
+    surface_game = pygame.transform.rotozoom(surface, 0, 0.5)
 
+    surface_bg.blit(surface_game, (0,self.game_height//2) )
 
+    surface_array : numpy.array = pygame.surfarray.array2d(surface_bg)
+    new_surfarray = Game.shear2(surface_array)
+    new_surface = pygame.surfarray.make_surface(new_surfarray)
 
-    surface = pygame.transform.rotozoom(surface, 30.0, 0.7)
-    self.gameDisplay.blit(surface, (0,0))
+    self.gameDisplay.blit(new_surface, (0,-self.game_height//4))
     pygame.display.flip()
 
 
 class Player:
-  def __init__(self, i, j):
-    self.i = i
-    self.j = j
-    self.rotation = 0
-  def update(self, direction, matrix):
 
-    new_i,new_j = self.i , self.j
-    
+  STANDING = 0
+  VERTICAL = 1
+  HORIZONTAL = 2
 
-    d = {
+  POSITIONS = {
+    0 : "STANDING",
+    1 : "VERTICAL",
+    2 : "HORIZONTAL"
+  }
+
+  MOVEMENT_KEYS = {
       pygame.K_LEFT: (0,-1),
       pygame.K_RIGHT: (0,1),
       pygame.K_UP: (-1,0),
       pygame.K_DOWN: (1,0)
     }
 
-    new_i,new_j = new_i + d[direction][0], new_j + d[direction][1]
+  def __init__(self, i, j):
+    self.i = i
+    self.j = j
+    self.rotation = Player.STANDING
+    
 
+  def check_valid_position(self, matrix, new_i, new_j, new_rotation) -> bool:
     m , n = len(matrix), len(matrix[0])
-    if new_i < 0:
-      new_i = 0
-    if new_i >= m:
-      new_i = m-1
-    if new_j < 0:
-      new_j = 0
-    if new_j >= n:
-      new_j = n - 1
 
-    if (new_i != self.i) or (new_j != self.j):
-      if matrix[new_i][new_j] > 0:
-        self.i, self.j = new_i, new_j
+    y1 = y2 = new_i
+    x1 = x2 = new_j
+
+    if new_rotation == Player.HORIZONTAL:
+      x2 = x1 + 1
+    if new_rotation == Player.VERTICAL:
+      y2 = y1 + 1
+  
+    if x1 < 0 or x2 >= m:
+      return False
+    if y1 < 0 or y2 >= n:
+      return False
+    if matrix[y1][x1] == 0 and matrix[y2][x2] == 0:
+      return False
+
+    return True
+
+  def update(self, direction, matrix):
 
 
-  def render(self, gameDisplay, scale):
+    new_i,new_j = self.i, self.j
+    new_rotation = self.rotation
+    new_movement = Player.MOVEMENT_KEYS[direction]
+
+
+    if new_rotation == Player.STANDING:
+      if direction == pygame.K_UP or direction == pygame.K_DOWN:
+        new_rotation = Player.VERTICAL
+
+      elif direction == pygame.K_LEFT or direction == pygame.K_RIGHT:
+        new_rotation = Player.HORIZONTAL
+
+      new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+      if direction == pygame.K_UP or direction == pygame.K_LEFT:
+        new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+    
+    elif new_rotation == Player.HORIZONTAL:
+      if direction == pygame.K_LEFT or direction == pygame.K_RIGHT:
+        new_rotation = Player.STANDING
+
+      new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+      if direction == pygame.K_RIGHT:
+        new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+    
+
+    elif new_rotation == Player.VERTICAL:
+      if direction == pygame.K_UP or direction == pygame.K_DOWN:
+        new_rotation = Player.STANDING
+
+      new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+      if direction == pygame.K_DOWN:
+        new_i,new_j = new_i + new_movement[0], new_j + new_movement[1]
+
+    
+    print(f'Rotation:{Player.POSITIONS[self.rotation]} -> {Player.POSITIONS[new_rotation]}')
+    
+    if self.check_valid_position(matrix, new_i, new_j, new_rotation):
+      self.i, self.j = new_i, new_j
+      self.rotation = new_rotation
+
+
+  def render(self, gameDisplay, scale : int):
     offset = scale//10
-    #rect = pygame.Rect(( self.j*scale , self.i*scale , scale, scale)) #Creo el rectangulo, x1, y1, w, h
-    rect = pygame.Rect(( offset + self.j*scale , offset + self.i*scale , scale-2*offset, scale-2*offset)) #Creo el rectangulo, x1, y1, w, h
+
+    width = scale-2*offset
+    height = scale-2*offset
+
+    if self.rotation == Player.HORIZONTAL:
+      width = (2*scale)-2*offset
+    elif self.rotation == Player.VERTICAL:
+      height = (2*scale)-2*offset
+    
+    rect = pygame.Rect(( offset + self.j*scale , offset + self.i*scale , width, height)) 
 
     pygame.draw.rect(gameDisplay,(150,150,0),rect ) #Dibujo el rectangulo
 
@@ -101,7 +182,8 @@ def main():
   clock = pygame.time.Clock() #inicializo el reloj para los FPS
   alive = True
   game = Game()
-  
+
+
   while alive:
 
     for event in pygame.event.get():
@@ -109,7 +191,7 @@ def main():
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key in (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN):
+            if event.key in Player.MOVEMENT_KEYS:
                 game.player.update(event.key, game.matrix)
             if event.key == pygame.K_SPACE:
                 alive= False
